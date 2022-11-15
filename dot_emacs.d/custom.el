ed37af526fc7e37e814ad5ea5d499243268c5453
Jackson Baker <Jacksonbaker323@Gmail.com>
2022-11-14 23:38:40 -0800

    updated zshrc to add ssh-agent

---
+++ dot_emacs.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(visual-fill-column ivy-rich which-key rainbow-delimiters doom-modeline counsel ivy command-log-mode doom-themes all-the-icons use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
